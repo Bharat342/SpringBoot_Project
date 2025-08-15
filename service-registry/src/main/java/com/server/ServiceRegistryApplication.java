c447b0b1267e6a49494bdd068fdc1ded17b06227
Bharat <bharatnaik342@gmail.com>
2025-08-15 13:02:10 +0530

    adding Service-Registry(Eureka Server)

--- service-registry/src/main/java/com/server/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/server/ServiceRegistryApplication.java
@@ -1,18 +1,16 @@
-package com.authservice;
+package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import com.authservice.entity.User;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AuthServiceApplication {   
+@EnableEurekaServer
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
